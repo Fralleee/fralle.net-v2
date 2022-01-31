6ef18a506f9cc712d65a57b5bc35ea2a5f54482b
Roland Chelwing <jolleee@hotmail.com>
2022-01-31 16:10:56 +0100

    Fix icons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 
 import BackgroundImage from "images/background.svg"
+import LogoImage from "images/logo.svg"
 import styled from "styled-components"
 
 const Header = styled.section`
   width: 100%;
-  height: 500px;
+  min-height: 500px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -13,9 +14,26 @@ const Header = styled.section`
   mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 0) 90%);
   background-image: ${() => `url(${BackgroundImage})`};
   background-size: calc(900px + 35vw);
+  background-repeat: no-repeat;
+`
+
+const Logo = styled.i`
+  position: absolute;
+  top: -1vh;
+  left: -3vw;
+  background-size: contain;
+  background-image: ${() => `url(${LogoImage})`};
+  background-repeat: no-repeat;
+  width: calc(20px + 2vw);
+  height: 100%;
+  text-shadow: none;
+  z-index: 2;
+  
 `
 
 const Title = styled.h1`
+  position: relative;
+  display: flex;
   z-index: 1;
   text-shadow: 
     -12px -12px 12px var(--background), 
@@ -33,7 +51,10 @@ const Subtitle = styled.h2`
 const App = () => {
   return (
     <Header>
-      <Title>Fralle</Title>
+      <Title>
+        <Logo />
+        Fralle
+      </Title>
       <Subtitle>Software developer & sassy home cook</Subtitle>
     </Header>
   )
