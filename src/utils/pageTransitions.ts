cfd560391c3de743035c125d0aa18b22b528c8b5
Roland Chelwing <jolleee@hotmail.com>
2022-02-10 11:44:01 +0100

    Add missing files

---
+++ src/utils/pageTransitions.ts
@@ -0,0 +1,13 @@
+export const browseTransitions = {
+  initial: { opacity: 0, scale: 0.8 }, // translateY?
+  animate: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 0.8 },
+  transition: { duration: 0.2 }
+}
+
+export const projectTransition = {
+  initial: { opacity: 0, scale: 0.8 },
+  animate: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 0.8 },
+  transition: { duration: 0.2 }
+}
